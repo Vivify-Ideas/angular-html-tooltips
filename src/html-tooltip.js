f8882168eda4a2859eb37a5ed58f3b757f4e9ddb
Aleksandar Nikolic <sasa.n@vivifyideas.com>
2017-05-25 11:53:49 +0200

    Removing dependancy on jQuery and lodash.

--- src/html-tooltip.js
+++ src/html-tooltip.js
@@ -18,62 +18,72 @@
         return {
             restrict: 'A',
             scope: {
-              tip: '@tooltip',
-              direction: '@tooltipDirection',
-              tooltipHide: '=?',
+              tip: '@viHtmlTooltip',
+              direction: '@viTooltipDirection',
+              viTooltipHide: '=?',
               width: '=?'
             },
-            link: function(scope, elem, attr) {
+            link: function(scope, element, attrs) {
 
               if (scope.tooltipHide) {
                 return;
               }
 
+              if (!scope.direction) {
+                scope.direction = 'left';
+              }
+
               var tooltip;
-              var appendToSelector = attrs.tooltipAppendTo || 'body';
-              var topOffset = angular.element(appendToSelector).offset().top;
+              var appendToElement = document.querySelector(attrs.tooltipAppendTo || 'body');
+              var topOffset = appendToElement.offsetTop;
               var timer = null;
               var horizontalOffset = 35;
               var verticalOffset = 20;
               var createTooltip = function() {
                 var tipClass = scope.direction ? 'tip-' + scope.direction : 'tip-left';
-                tipClass += ' html-tooltip';
-                var style = scope.width ? 'style="max-width:' + scope.width + 'px" ' : '';
-                tooltip = angular.element('<div '+ style +'class="' + tipClass + '"></div>');
-
-                tooltip.appendTo(appendToSelector);
-                tooltip.append('<div class="tip"></div>');
-                tooltip.append('<div class="content"></div>');
-                tooltip.find('.content').append($compile(scope.tip)(scope));
+                tooltip = document.createElement('div');
+                tooltip.classList.add('html-tooltip');
+                tooltip.classList.add(tipClass);
+                if (scope.width) {
+                  tooltip.style.maxWidth = scope.width + 'px';
+                }
+                var tip = document.createElement('div');
+                tip.classList.add('tip');
+                var content = document.createElement('div');
+                content.classList.add('content');
+                tooltip.append(tip);
+                tooltip.append(content);
+                appendToElement.append(tooltip);
+                content.insertAdjacentHTML('beforeend', scope.tip);
+                $compile(content)(scope);
               };
 
               createTooltip();
 
               var positionTooltip = function() {
-                var offset = element.offset();
                 switch (scope.direction) {
                   case 'left':
                     tooltip.css({
-                      left: offset.left - tooltip.outerWidth() - horizontalOffset + 'px',
-                      top: offset.top - (tooltip.outerHeight() / 2) + (element.outerHeight() / 2) + 'px',
+                      left: element.offsetLeft - tooltip.outerWidth() - horizontalOffset + 'px',
+                      top: element.offsetHeight - (tooltip.outerHeight() / 2) + (element.outerHeight() / 2) + 'px',
                     });
                     break;
                   case 'right':
                     tooltip.css({
-                      left: offset.left + element.outerWidth() + horizontalOffset + 'px',
-                      top: offset.top - (tooltip.outerHeight() / 2) + (element.outerHeight() / 2) - topOffset + 'px',
+                      left: element.offsetLeft + element.outerWidth() + horizontalOffset + 'px',
+                      top: element.offsetHeight - (tooltip.outerHeight() / 2) + (element.outerHeight() / 2) - topOffset + 'px',
                     });
                     break;
                   case 'top':
                     tooltip.css({
-                      left: offset.left + (element.outerWidth() / 2) - (tooltip.outerWidth() / 2) + 'px',
-                      top: offset.top - tooltip.outerHeight() - verticalOffset + 'px',
+                      left: element.offsetLeft + (element.outerWidth() / 2) - (tooltip.outerWidth() / 2) + 'px',
+                      top: element.offsetHeight - tooltip.outerHeight() - verticalOffset + 'px',
                     });
                     break;
                   case 'bottom':
                     tooltip.css({
-                      left: offset.left + (element.outerWidth() / 2) - (tooltip.outerWidth() / 2) + 'px',
-                      top: offset.top + element.outerHeight() + verticalOffset + 'px',
+                      left: element.offsetLeft + (element.outerWidth() / 2) - (tooltip.outerWidth() / 2) + 'px',
+                      top: element.offsetHeight + element.outerHeight() + verticalOffset + 'px',
                     });
                     break;
                 }
@@ -85,16 +95,16 @@
                 }
                 $timeout.cancel(timer);
                 positionTooltip();
-                tooltip.addClass('display');
+                tooltip.classList.add('display');
               };
 
               var hideTooltip = function() {
                 timer = $timeout(function() {
-                  tooltip.removeClass('display');
+                  tooltip.classList.remove('display');
                 }, 500);
               };
 
-              var alwaysOnTop = !_.isUndefined(attrs.tooltipAlwaysOn);
+              var alwaysOnTop = "undefined" !== typeof attrs.tooltipAlwaysOn;
 
               if (alwaysOnTop) {
                 $timeout(function() {
@@ -118,8 +128,8 @@
               });
 
               if (!scope.tooltipHide && !alwaysOnTop) {
-                tooltip.on('mouseover', showTooltip);
-                tooltip.on('mouseout', hideTooltip);
+                // tooltip.on('mouseover', showTooltip);
+                // tooltip.on('mouseout', hideTooltip);
                 element.on('mouseover', showTooltip);
                 element.on('mouseout', hideTooltip);
                 element.on('mousemove', positionTooltip);
